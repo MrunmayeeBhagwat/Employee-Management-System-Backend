94e08ba0a330d42deed26f64aa2f1ed0cc161c97
MrunmayeeBhagwat <mrunmayeebhagwat3@gmail.com>
2022-04-06 21:43:01 +0530

    Initial commit for backend

---
+++ src/main/java/com/mrunmayee/springboot/exception/ResourceNotFoundException.java
@@ -0,0 +1,16 @@
+package com.mrunmayee.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;  //RuntimeException implements serializable interface
+	
+	public ResourceNotFoundException(String message)
+	{
+		super(message);
+	}
+
+}
